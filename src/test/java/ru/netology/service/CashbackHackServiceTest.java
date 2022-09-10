74b91bdab9b3429eee1d819c5687e1bb4834ea32
julyy5 <smirnova-1999@inbox.ru>
2022-09-10 15:59:10 +0300

    testNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testLess1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1;
+        int actual = service.remain(999);
+        assertEquals(actual,expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void test1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1000;
+        int actual = service.remain(1000);
+        assertEquals(actual,expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void testMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int actual = service.remain(1001);
+        assertEquals(actual,expected);
+
+    }
+}
